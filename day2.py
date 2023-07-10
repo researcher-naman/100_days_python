56ae5b41c2851426336791baf4037c72c0ccf9d1
Namanqw1 <nasuchacha1708@gmail.com>
2023-07-10 19:41:03 +0530

    day3-4

---
+++ day2.py
@@ -0,0 +1,1 @@
+#I tryed all 5 code but dose not work in my pc i was unble to intsall tha outside libs.
---
+++ day3-4.py
@@ -0,0 +1,37 @@
+# auto username genrating program
+
+# you have to enter the you details here
+
+my_name = "Naman"
+birth_date = "17"
+birth_day = "sat"
+birth_year = "2002"
+
+print("This is my day3-4 python program.")
+
+print("  ") #space bwt the programming console
+
+print("Python program which genrate a 5 username based on povided data")
+
+print("  ")
+
+print("Given data...")
+print("Your Name : " + my_name)
+print("Birth date : " + birth_date + " " + birth_day + " " + birth_year )
+
+print("  ")
+
+print("Gentrating username.......")
+
+print("  ")
+
+print(my_name + birth_date)
+print(my_name + birth_year)
+print(my_name + "qw")
+print("self." + my_name)
+print(my_name + "_" + birth_date)
+
+
+
+
+
