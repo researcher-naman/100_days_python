520ae53fae46622d7e614bb42dadca963d8801cb
Namanqw1 <nasuchacha1708@gmail.com>
2023-07-12 13:00:22 +0530

    day3-4

--- day2.py
+++ day2.py
@@ -1,1 +1,2 @@
 #I tryed all 5 code but dose not work in my pc i was unble to intsall tha outside libs.
+
--- day3-4.py
+++ day3-4.py
@@ -1,17 +1,18 @@
-# auto username genrating program
-
-# you have to enter the you details here
-
-my_name = "Naman"
-birth_date = "17"
-birth_day = "sat"
-birth_year = "2002"
-
+# auto password genrating program
 print("This is my day3-4 python program.")
 
 print("  ") #space bwt the programming console
 
-print("Python program which genrate a 5 username based on povided data")
+print("Python program which genrate a list of password based on povided data")
+print("  ")
+
+# you have to enter the you details here
+
+my_name = input("Enter Your Name: ")
+print("Now add your birth date as asked")
+birth_date = input("Your Birth Date : ")
+birth_day = input("Your Birth Day : ")
+birth_year = input("Your Birth Year : ")
 
 print("  ")
 
@@ -21,7 +22,7 @@ print("Birth date : " + birth_date + " " + birth_day + " " + birth_year )
 
 print("  ")
 
-print("Gentrating username.......")
+print("Gentrating password.......")
 
 print("  ")
 
@@ -30,8 +31,16 @@ print(my_name + birth_year)
 print(my_name + "qw")
 print("self." + my_name)
 print(my_name + "_" + birth_date)
-
-
-
-
+print(my_name + birth_date + "@" + birth_year)
+print(my_name + "@" + birth_year)
+print("myself_" + my_name)
+print("self_" + my_name)
+print("decode@" + my_name)
+print("CodeWith" + my_name)
+print("LreanWith" + my_name)
+print(my_name + "_" + birth_year)
+print(my_name + birth_year + "@" + birth_date)
+print(my_name + "@" + birth_date)
+print("stud_" + my_name)
+print("scrcastic_" + my_name)
 
