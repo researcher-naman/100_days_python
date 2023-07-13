ae4842ec7dcab8b70ab95937f0d27f4b64718509
Namanqw1 <nasuchacha1708@gmail.com>
2023-07-13 12:04:02 +0530

    day7 done

---
+++ day7.py
@@ -0,0 +1,16 @@
+# DAY 7 PYTHON OPREATORS
+
+# CWH EX1 (MAKE A CALCULATOR USING PYTHON)
+
+print("PYTHON CALCULATOR\n")
+
+a = 5
+b = 7
+
+print("\n Addition of ", a ," and ", b," is ", a+b)
+print("\n Subtraction of ", a ," with ", b," is ", a-b)
+print("\n Multiplication of ", a ," with ", b," is ", a*b)
+print("\n Division of ", a ," by ", b," is ", a/b)
+print("\n Floor Division of ", a ," by ", b," is ", a//b)
+print("\n Modulus of ", a ," by ", b," is ", a%b)
+print("\n Exponential of ", a ," to ", b," is ", a**b)
