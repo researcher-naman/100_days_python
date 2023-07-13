94453c83cdc9be3f33d3744a25a46ea756800031
Namanqw1 <nasuchacha1708@gmail.com>
2023-07-13 11:47:51 +0530

    day6 done

---
+++ day6.py
@@ -0,0 +1,46 @@
+# VARIABLES IN PYTHONS
+
+#types of varioable inpython are 
+"""
+1. int
+2. floot
+3. boolean
+4. string
+"""
+
+a = 1
+b = 1.555
+c = True
+d = "nasu"
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+# SOME EXT DATA TYPES IN PYTHON ARE
+
+# 1. LIST (mutable: means it can be modified by the cmd of python)
+
+list1 = [1,2.55, ["nasu","naman jain"]]
+print(list1)
+
+# 2. TUPLE (Immutabl: means it can't be change using python cmd once it has been made)
+
+Tuple1 = (1,5.33, ("vinay","vinu"))
+print(Tuple1)
+
+# 3. Mapped data or Dict (it store value with as key:value pairs same like arrays)
+
+dict = {"name:naman","age:21","filed:computer sci"}
+print(dict)
+
+# now how we know the data type of any variable in pythons?
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(list1))
+print(type(Tuple1))
+print(type(dict))
