ad1e1f2c3aa4020dd1c793870e847020343c9deb
Namanqw1 <nasuchacha1708@gmail.com>
2023-07-13 15:17:57 +0530

    day9-10 done

---
+++ day9-10.py
@@ -0,0 +1,19 @@
+"""
+PYTHON DAY 9 ADN 10
+TYPE CASTING AND USER INPUT IN PYTHON
+
+NOW I WILL MAKE MY CALCULATOR PROGRAM WITCH CAN TAKE USER INPUT
+"""
+
+print("PYTHON CALCULATOR\n")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("\n Addition of", a ,"and", b,"is", a+b)
+print("\n Subtraction of", a ,"with", b,"is", a-b)
+print("\n Multiplication of", a ,"with", b,"is", a*b)
+print("\n Division of", a ,"by", b,"is", a/b)
+print("\n Floor Division of", a ,"by", b,"is", a//b)
+print("\n Modulus of", a ,"by", b,"is", a%b)
+print("\n Exponential of", a ,"to", b,"is", a**b)
