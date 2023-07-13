840bb5143007f15e15ec4eaf737d50528429171f
Namanqw1 <nasuchacha1708@gmail.com>
2023-07-13 11:01:37 +0530

    day5 done

---
+++ day5.py
@@ -0,0 +1,28 @@
+#Python day5 Escape Seq Char
+
+print ("hey good morning I am a good Boy\nWhat kind of are you?")
+
+# sep="your_any_syntax" will do that i will add "your_any_syntax" bwt every element in print.
+# end="your_any_syntax" will do that it will add "your_any_syntax" to end of last element in print.
+# also this attribute not complsary to use woth print.
+
+print("Hey",6,7, sep="~",end="nasu\n")
+
+
+print("hey","what","are","you","doing","here?", sep="\n", end="\nI am nasu...")
+
+"""
+this is muliti line comment
+what are doing here
+this will be not executed by python
+"""
+
+#how we can use ("") in print cmd ?
+
+# ans: by using Escape seq char
+
+print("hey this is my name \"naman jain\" ")
+
+# also we can use ('') in print cmd using same \'
+
+print("This is my nickname \'Nasu\'")
