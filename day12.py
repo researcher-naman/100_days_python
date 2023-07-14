b9d6dfb20fb04bdf1caa0b519c9762504ea864d1
Namanqw1 <nasuchacha1708@gmail.com>
2023-07-14 12:22:29 +0530

    day12 done

---
+++ day12.py
@@ -0,0 +1,25 @@
+# PYTHON DAY 12 STR (OPREATIONS AND SLICING)
+
+# how we can know the total length of string in python
+# ans: by using len() function
+
+name = "naman jain"
+print(len(name))
+
+# How to slici the string
+# ans: using name[startid : endid]
+
+print(name[0:5]) 
+# same
+print(name[:5])
+
+# wecan also write negative values
+# which means -3 = len(name)-3
+
+print(name[0:-3])
+print(name[0:len(name)-3])
+print(name[0:7])
+
+#all this 3 are same
+
+
