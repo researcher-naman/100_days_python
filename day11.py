f2ea209213f3e88e27faba52b99bdb9f121e668c
Namanqw1 <nasuchacha1708@gmail.com>
2023-07-14 11:55:18 +0530

    day11 done

---
+++ day11.py
@@ -0,0 +1,26 @@
+# PYTHON DAY 11 
+
+# STRINGS IN PYTHON
+
+name = "naman jain"
+
+# How to write a muliti line string
+
+words = """
+hii my name is naman jain
+I am also know as 'nasu'
+which is a 21 year "BCA" graduated student
+and very personate about me computer sci future"""
+
+print(words)
+
+# index in pythons
+# In python every string behaves like an array means it char has some index
+
+# Which means name = "naman jain" also has char with there index
+# also as other language pyhton index also start form 0
+
+# how to use index in python
+
+print(name[0])
+print(name[1])
