3385d7c0ce8e441eeccb6894d80fc296e4b30d84
Namanqw1 <nasuchacha1708@gmail.com>
2023-08-09 22:10:21 +0530

    done

--- extra/bisection.py
+++ extra/bisection.py
@@ -1,17 +1,17 @@
-# Python program to find the roots by itrative method or bisection method
+# Python program to find the roots by Itrative / Bisection method
 
 print("BISECTION METHOD\n")
 
 # Making equtaion
 
 def maineqx(x):
-    return x*x*x - 4*x - 9
+    return x*x*x*x - x*x*x - x*x -4
 
 # Asking user to assing the range
 
 a = int(input("Enter The -ve Number: ")) # taking -ve number
 b = int(input("Enter The +ve Number: ")) # taking +ve number
-n = int(input("Enter the itration you have to find : ")) # Asking user how many time itration is to be done
+n = int(input("Enter the Itration you have to find : ")) # Asking user how many time itration is to be done
 
 def bisection(a,b):
     # Step 1st
@@ -20,7 +20,7 @@ def bisection(a,b):
     
     # Step 2nd
     c_value = maineqx(c)
-    print("\nSo the value of f(" + str(c) + ") is " + str(c_value) + "\n")
+    print("So the value of f(" + str(c) + ") is " + str(c_value))
     
     # Step 3rd
     if (c_value < 0):
@@ -35,10 +35,10 @@ def bisection(a,b):
         print("now new value of b is " + str(b))
     return (a,b)
 
-for i in range(1, n):
+for i in range(1, n+1):
     print("\nItration Number:" + str(i) + "\n")
+    print("-------------------------------------")
     a,b = bisection(a,b)
-    bisection(a,b)
+    print("-------------------------------------")
     
-    
-    
+print("\nSo the final root for this funtion is " + str(a) + " or " + str(b))
