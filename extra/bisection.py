ba532dbaaceece9bea1039ae073292aa5165dfda
Namanqw1 <nasuchacha1708@gmail.com>
2023-08-10 07:46:44 +0530

    minor changes

--- extra/bisection.py
+++ extra/bisection.py
@@ -10,7 +10,30 @@ def maineqx(x):
 # Asking user to assing the range
 
 a = int(input("Enter The -ve Number: ")) # taking -ve number
+
+# Checking wethere the user entered right value of a
+
+if (maineqx(a)<0):
+    print("f(a) = " + str(maineqx(a)))
+    print("The value of a is set to " + str(a))
+else:
+    print("f(a) = " + str(maineqx(a)))
+    print("Your value is wrong as f(a) output is +ve")
+    exit()
+    
 b = int(input("Enter The +ve Number: ")) # taking +ve number
+
+# Checking wethere the user entered right value of b
+
+if (maineqx(b)>0):
+    print("f(b) = " + str(maineqx(b)))
+    print("The value of b is set to " + str(b))
+else:
+    print("f(b) = " + str(maineqx(b)))
+    print("Your value is worng as f(b) output is -ve")
+    exit()
+
+# Checking a main condition
 
 if (maineqx(a)*maineqx(b)>0):
     print("Your data is Wrong")
@@ -46,6 +69,10 @@ for i in range(1, n+1):
     print("\nItration Number:" + str(i) + "\n")
     print("-------------------------------------")
     a,b = bisection(a,b)
+    myexit = int(input("Enter 1 for exit and 0 next Itration"))
+    if (myexit == 1):
+        print("\nSo the final root for this funtion is " + str(a) + " or " + str(b) + " at Itration no. " + str(i))
+        exit()
     print("-------------------------------------")
     
-print("\nSo the final root for this funtion is " + str(a) + " or " + str(b))
+print("\nSo the final root for this funtion is " + str(a) + " or " + str(b) + " at Itration no. " + str(i))
--- extra/falsepos.py
+++ extra/falsepos.py
@@ -10,7 +10,30 @@ def maineqx(x):
 # Asking user to assing the range
 
 a = int(input("Enter The -ve Number: ")) # taking -ve number
+
+# Checking wethere the user entered right value of a
+
+if (maineqx(a)<0):
+    print("f(a) = " + str(maineqx(a)))
+    print("The value of a is set to " + str(a))
+else:
+    print("f(a) = " + str(maineqx(a)))
+    print("Your value is wrong as f(a) output is +ve")
+    exit()
+    
 b = int(input("Enter The +ve Number: ")) # taking +ve number
+
+# Checking wethere the user entered right value of b
+
+if (maineqx(b)>0):
+    print("f(b) = " + str(maineqx(b)))
+    print("The value of b is set to " + str(b))
+else:
+    print("f(b) = " + str(maineqx(b)))
+    print("Your value is worng as f(b) output is -ve")
+    exit()
+
+# Checking a main condition
 
 if (maineqx(a)*maineqx(b)>0):
     print("Your data is Wrong")
@@ -26,12 +49,16 @@ def false_pos(a,b):
     # STEP 1 for sloving the formula and find the value of c
     
     half_1 = b-a 
+    # print(half_1)
     
     half_2 = maineqx(b) - maineqx(a)
+    # print(half_2)
     
     half_12 = half_1 / half_2
+    # print(half_12)
     
     half_3 = maineqx(a) * half_12
+    # print(half_3)
     
     c = a - half_3
     
@@ -61,8 +88,12 @@ for i in range(1, n+1):
     print("\nItration Number:" + str(i) + "\n")
     print("-------------------------------------")
     a,b = false_pos(a,b)
+    myexit = int(input("Enter 1 for exit and 0 for next Itration"))
+    if (myexit == 1):
+        print("\nSo the final root for this funtion is " + str(a) + " or " + str(b) + " at Itration no. " + str(i))
+        exit()
     print("-------------------------------------")
     
-print("\nSo the final root for this funtion is " + str(a) + " or " + str(b))
+print("\nSo the final root for this funtion is " + str(a) + " or " + str(b) + " at Itration no. " + str(i))
     
     
